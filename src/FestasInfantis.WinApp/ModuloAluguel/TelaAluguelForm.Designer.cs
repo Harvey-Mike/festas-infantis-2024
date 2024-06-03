a7546174b3eb39d8db895576aefaed4ac3b87711
ISRAEL FANTONI <127950758+Israel-ux440@users.noreply.github.com>
2024-06-03 17:47:35 -0300

    implementando o cadastro de aluguéis

---
+++ src/FestasInfantis.WinApp/ModuloAluguel/TelaAluguelForm.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace FestasInfantis.WinApp.ModuloAluguel
+{
+    partial class TelaAluguelForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            groupBox2 = new GroupBox();
+            label6 = new Label();
+            label7 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label5 = new Label();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox9 = new TextBox();
+            textBox8 = new TextBox();
+            textBox5 = new TextBox();
+            groupBox1 = new GroupBox();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Location = new Point(67, 23);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(54, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(41, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Id:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 52);
+            label2.Name = "label2";
+            label2.Size = new Size(98, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Horário de Início:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(127, 15);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(40, 23);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Data da Festa:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(127, 44);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(9, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 15);
+            label4.TabIndex = 2;
+            label4.Text = "Horário de Término:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(127, 71);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 66);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(534, 396);
+            tabControl1.TabIndex = 4;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(groupBox2);
+            tabPage1.Controls.Add(groupBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(526, 368);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Dados da Festa";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(label6);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Controls.Add(label9);
+            groupBox2.Controls.Add(label8);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(textBox6);
+            groupBox2.Controls.Add(textBox7);
+            groupBox2.Controls.Add(textBox9);
+            groupBox2.Controls.Add(textBox8);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Location = new Point(6, 151);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(493, 149);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Dados do Endereço";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(20, 54);
+            label6.Name = "label6";
+            label6.Size = new Size(30, 15);
+            label6.TabIndex = 2;
+            label6.Text = "Rua:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(243, 25);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 15);
+            label7.TabIndex = 2;
+            label7.Text = "Estado:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(253, 83);
+            label9.Name = "label9";
+            label9.Size = new Size(54, 15);
+            label9.TabIndex = 2;
+            label9.Text = "Número:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(9, 83);
+            label8.Name = "label8";
+            label8.Size = new Size(41, 15);
+            label8.TabIndex = 2;
+            label8.Text = "Bairro:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(3, 25);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Cidade:";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(56, 51);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(431, 23);
+            textBox6.TabIndex = 3;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(306, 22);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(181, 23);
+            textBox7.TabIndex = 3;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(306, 80);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(181, 23);
+            textBox9.TabIndex = 3;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(56, 80);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(181, 23);
+            textBox8.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(56, 22);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(181, 23);
+            textBox5.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(3, 6);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(496, 139);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Data e Hora:";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(526, 368);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Dados do Alguel";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // TelaAluguelForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(557, 485);
+            Controls.Add(tabControl1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaAluguelForm";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "O cadastro de Aluguéis";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox2;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private Label label6;
+        private Label label7;
+        private Label label9;
+        private Label label8;
+        private Label label5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox9;
+        private TextBox textBox8;
+        private TextBox textBox5;
+    }
+}
--- src/FestasInfantis.WinApp/ModuloTema/Tema.cs
+++ src/FestasInfantis.WinApp/ModuloTema/Tema.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FestasInfantis.WinApp.ModuloTema
+﻿namespace FestasInfantis.WinApp.ModuloTema
 {
     internal class Tema
     {
+
     }
 }
+
