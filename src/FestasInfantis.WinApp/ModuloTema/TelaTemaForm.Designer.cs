b2b12dc5423e49278fb3afddcdc75776259e1f2b
Slaviero <slaviero.kaua@gmail.com>
2024-06-04 16:11:13 -0300

    Concluido tela de cadastro de temas

---
+++ src/FestasInfantis.WinApp/ModuloTema/TelaTemaForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace FestasInfantis.WinApp.ModuloTema
+{
+    partial class TelaTemaForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            btnGravar = new Button();
+            btnCancelar = new Button();
+            label1 = new Label();
+            txtId = new TextBox();
+            txtNome = new TextBox();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 24);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(492, 315);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(txtNome);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(txtId);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(484, 287);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Dados Básicos";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(listBox1);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(484, 287);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Seleção de Itens";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            btnGravar.Location = new Point(311, 354);
+            btnGravar.Name = "btnGravar";
+            btnGravar.Size = new Size(89, 44);
+            btnGravar.TabIndex = 3;
+            btnGravar.Text = "Gravar";
+            btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Location = new Point(411, 354);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(89, 44);
+            btnCancelar.TabIndex = 4;
+            btnCancelar.Text = "Cancelar";
+            btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(54, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(20, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Id:";
+            // 
+            // txtId
+            // 
+            txtId.Enabled = false;
+            txtId.Location = new Point(80, 26);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(61, 23);
+            txtId.TabIndex = 1;
+            txtId.Text = "0";
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(80, 63);
+            txtNome.Name = "txtNome";
+            txtNome.Size = new Size(304, 23);
+            txtNome.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(36, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(43, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Nome:";
+            // 
+            // listBox1
+            // 
+            listBox1.Dock = DockStyle.Fill;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(3, 3);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(478, 281);
+            listBox1.TabIndex = 0;
+            // 
+            // TelaTemaForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(516, 410);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnGravar);
+            Controls.Add(tabControl1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaTemaForm";
+            ShowIcon = false;
+            Text = "Cadastro de Temas";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private TabPage tabPage2;
+        private Button btnGravar;
+        private Button btnCancelar;
+        private TextBox txtNome;
+        private Label label2;
+        private TextBox txtId;
+        private ListBox listBox1;
+    }
+}
